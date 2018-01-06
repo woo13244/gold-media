6d6132fcf4a70a2677250b59daf265ad1b761116
Woo Lee <woo@Woos-MacBook-Air.local>
2018-01-06 01:13:41 -0500

    third render

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,26 +7,58 @@ import {HashLink as Slink} from 'react-router-hash-link';
 
 
 export default class Header extends Component{
+    constructor(){
+        super()
+        this.state={
+            menu : false
+        }
+    }
     render(){
         return(
             <div className="Header">
-                <Link className="logo" to="/"><span>GOLD</span>MEDIA</Link>
-                <div className="headerContent">
-                    <div>
-                        <Link className="bert" to="/aboutus">ABOUT US</Link>
+                <div className="headerContainer">
+                    <div className="headerLogo"><Link className="logo" to="/"><span>GOLD</span>MEDIA</Link></div>
+                    <div className="headerLeft">
+                        <div><Link className="bert" to="/aboutus">ABOUT US</Link></div>
+                        <div><Link className="bert" to="/ourteam">OUR TEAM</Link></div>
+                        <div><Link className="bert" to="/ourimpact">OUR IMPACT</Link></div>
+                        <div ><Slink to='/#becomeAClient' className="becomeAClient">BECOME A CLIENT</Slink></div>
                     </div>
-                    <div>
-                        <Link className="bert" to="/ourteam">OUR TEAM</Link>
-                    </div>
-                    <div>
-                        <Link className="bert" to="/ourimpact">OUR IMPACT</Link>
-                    </div>
-                    <Slink to="/#becomeAClient"><div className="becomeAClient" >BECOME A CLIENT</div></Slink>
+                    <div className="menuContainer" onClick={()=>{this.setState({menu : !this.state.menu})}}><FontAwesome name="bars fa-lg" className='menu'/></div>
                 </div>
-                {/* <div className="menuBar">
-                    <FontAwesome name="bars fa-lg"/>
-                </div> */}
+                <div className={this.state.menu ? 'menuExtension' : 'menuRetract'}>
+                    <div><Link className="bert" to="/aboutus">ABOUT US</Link></div>
+                    <div><Link className="bert" to="/ourteam">OUR TEAM</Link></div>
+                    <div><Link className="bert" to="/ourimpact">OUR IMPACT</Link></div>
+                </div>
             </div>
         )
     }
 }
+
+
+                {/* <div className="Header2">
+                    <Link className="logo" to="/"><span>GOLD</span>MEDIA</Link>
+                    <div className="headerContent">
+                        <div>
+                            <Link className="bert" to="/aboutus">ABOUT US</Link>
+                        </div>
+                        <div>
+                            <Link className="bert" to="/ourteam">OUR TEAM</Link>
+                        </div>
+                        <div>
+                            <Link className="bert" to="/ourimpact">OUR IMPACT</Link>
+                        </div>
+                        <Slink to="/#becomeAClient"><div className="becomeAClient" >BECOME A CLIENT</div></Slink>
+                    </div>
+                    <div className="menuBar" onClick={()=>{
+                        this.setState({
+                            menu : !this.state.menu
+                        },()=>{
+                            console.log(this.state.menu)
+                        })
+                    }}>
+                        <FontAwesome name="bars fa-lg"/>
+                    </div>
+                </div>
+                <div style={{marginTop: 100, border: '2px solid red'}}>yelo</div> */}
